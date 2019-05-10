ba449dbf2d6401a16f929a9375ff72a6303d252e
Sampath17 <iamsampath17@gmail.com>
2019-05-10 10:58:00 +0530

    Python Basics

---
+++ Demo/PythonBasics.py
@@ -0,0 +1,27 @@
+print("Hello world, This is TechByteCoder!")
+
+# Variables
+
+x = 10;
+y = 20;
+z = x + y
+print(z)
+
+# syntax
+
+a = 10;
+b = "Automation";
+print ("Hello Sam " + b)
+
+
+
+if 10>5:
+    print("10 is greater than 5")
+
+
+
+# Functions
+def sum(a,b):
+    print(a+b)
+
+x = sum(20,30);
