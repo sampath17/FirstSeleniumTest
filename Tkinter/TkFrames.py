3b18d78feb286e60d4692c9d0604c3eca89ffb5f
Sampath17 <iamsampath17@gmail.com>
2019-05-13 10:24:58 +0530

    Tkinter - working on Frames by creating 2 frames and 2 buttons

---
+++ Tkinter/TkFrames.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+root = Tk()
+
+newframe = Frame(root)
+newframe.pack()
+
+otherframe = Frame(root)
+otherframe.pack(side=BOTTOM)
+
+button1 = Button(newframe, text="click here", fg="Red")
+button2 = Button(otherframe, text="click here", fg="Blue")
+
+button1.pack()
+button2.pack()
+
+root.mainloop()
+
+
